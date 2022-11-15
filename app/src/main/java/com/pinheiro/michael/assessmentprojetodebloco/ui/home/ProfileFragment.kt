eed6592e1a89b4dfb6ed8b74900fdb92e9c0b861
Daniel Nascimento <pdaniel.contato@gmail.com>
2022-11-15 15:02:40 -0300

    refactor profile layout

--- app/src/main/java/com/pinheiro/michael/assessmentprojetodebloco/ui/home/ProfileFragment.kt
+++ app/src/main/java/com/pinheiro/michael/assessmentprojetodebloco/ui/home/ProfileFragment.kt
@@ -4,7 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
+import com.pinheiro.michael.assessmentprojetodebloco.R
 import com.pinheiro.michael.assessmentprojetodebloco.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
@@ -20,6 +30,25 @@ class ProfileFragment : Fragment() {
 
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
 
+        binding.cvDecks.setContent {
+            val lista = listOf("Deck 1", "Deck 2", "Deck 3", "Deck 4", "Deck 5")
+            LazyColumn(modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)) {
+                items(lista.size) { index ->
+                    Card(modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp), backgroundColor = colorResource(
+                        id = R.color.cian_dark
+                    )) {
+                        Text(text = lista[index].toString(), modifier = Modifier.padding(32.dp))
+                    }
+                }
+            }
+
+
+        }
+        
         return binding.root
     }
 
